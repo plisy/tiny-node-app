4f659912103b23a68603c253b18e85b75fcdfacf
Peter Lisy <plisy@amazon.com>
2024-03-18 16:33:33 -0700

    Added Cache-Control header

--- .amplify-hosting/compute/default/server.js
+++ .amplify-hosting/compute/default/server.js
@@ -6,6 +6,7 @@ const server = http.createServer((req, res) => {
 
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Cache-Control', 'private, no-cache, no-store, max-age=0, s-maxage=0, must-revalidate');
   res.end();
 });
 
